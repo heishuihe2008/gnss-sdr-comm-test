932e5afb497c9caadaf83304721e2c2741d48bd1
Heishui He <heishuihe2008@qq.com>
2020-05-29 10:17:31 +0800

    Add files via upload

---
+++ uhd_find_devices_log.sh
@@ -0,0 +1,26 @@
+linux; GNU C++ version 5.3.1 20151219; Boost_105800; UHD_003.009.002-0-unknown
+
+--------------------------------------------------
+-- UHD Device 0
+--------------------------------------------------
+Device Address:
+    type: x300
+    addr: 192.168.10.2
+    fpga: HGS
+    name: 
+    serial: 314DFBF
+    product: X310
+
+
+--------------------------------------------------
+-- UHD Device 1
+--------------------------------------------------
+Device Address:
+    type: x300
+    addr: 192.168.10.2
+    fpga: HGS
+    name: 
+    serial: 314DFBF
+    product: X310
+
+
---
+++ uhd_usrp_probe_log.sh
@@ -0,0 +1,141 @@
+linux; GNU C++ version 5.3.1 20151219; Boost_105800; UHD_003.009.002-0-unknown
+
+-- X300 initialization sequence...
+-- Determining maximum frame size... 1472 bytes.
+-- Setup basic communication...
+-- Loading values from EEPROM...
+-- Setup RF frontend clocking...
+-- Radio 1x clock:200
+-- Detecting internal GPSDO.... Found an internal GPSDO
+-- Initialize Radio0 control...
+-- Performing register loopback test... pass
+-- Initialize Radio1 control...
+-- Performing register loopback test... pass
+  _____________________________________________________
+ /
+|       Device: X-Series Device
+|     _____________________________________________________
+|    /
+|   |       Mboard: X310
+|   |   revision: 11
+|   |   revision_compat: 7
+|   |   product: 30809
+|   |   mac-addr0: 00:80:2f:18:ee:7e
+|   |   mac-addr1: 00:80:2f:18:ee:7f
+|   |   gateway: 192.168.10.1
+|   |   ip-addr0: 192.168.10.2
+|   |   subnet0: 255.255.255.0
+|   |   ip-addr1: 192.168.20.2
+|   |   subnet1: 255.255.255.0
+|   |   ip-addr2: 192.168.30.2
+|   |   subnet2: 255.255.255.0
+|   |   ip-addr3: 192.168.40.2
+|   |   subnet3: 255.255.255.0
+|   |   serial: 314DFBF
+|   |   FW Version: 4.0
+|   |   FPGA Version: 19.0
+|   |   
+|   |   Time sources: internal, external, gpsdo
+|   |   Clock sources: internal, external, gpsdo
+|   |   Sensors: gps_gpgga, gps_gprmc, gps_time, gps_locked, gps_servo, ref_locked
+|   |     _____________________________________________________
+|   |    /
+|   |   |       RX DSP: 0
+|   |   |   Freq range: -100.000 to 100.000 MHz
+|   |     _____________________________________________________
+|   |    /
+|   |   |       RX DSP: 1
+|   |   |   Freq range: -100.000 to 100.000 MHz
+|   |     _____________________________________________________
+|   |    /
+|   |   |       RX Dboard: A
+|   |   |   ID: CBX (0x0067)
+|   |   |   Serial: 313BF42
+|   |   |     _____________________________________________________
+|   |   |    /
+|   |   |   |       RX Frontend: 0
+|   |   |   |   Name: CBX RX
+|   |   |   |   Antennas: TX/RX, RX2, CAL
+|   |   |   |   Sensors: lo_locked
+|   |   |   |   Freq range: 1200.000 to 6000.000 MHz
+|   |   |   |   Gain range PGA0: 0.0 to 31.5 step 0.5 dB
+|   |   |   |   Bandwidth range: 40000000.0 to 40000000.0 step 0.0 Hz
+|   |   |   |   Connection Type: IQ
+|   |   |   |   Uses LO offset: No
+|   |   |     _____________________________________________________
+|   |   |    /
+|   |   |   |       RX Codec: A
+|   |   |   |   Name: ads62p48
+|   |   |   |   Gain range digital: 0.0 to 6.0 step 0.5 dB
+|   |     _____________________________________________________
+|   |    /
+|   |   |       RX Dboard: B
+|   |   |   ID: CBX (0x0067)
+|   |   |   Serial: 313BF3C
+|   |   |     _____________________________________________________
+|   |   |    /
+|   |   |   |       RX Frontend: 0
+|   |   |   |   Name: CBX RX
+|   |   |   |   Antennas: TX/RX, RX2, CAL
+|   |   |   |   Sensors: lo_locked
+|   |   |   |   Freq range: 1200.000 to 6000.000 MHz
+|   |   |   |   Gain range PGA0: 0.0 to 31.5 step 0.5 dB
+|   |   |   |   Bandwidth range: 40000000.0 to 40000000.0 step 0.0 Hz
+|   |   |   |   Connection Type: IQ
+|   |   |   |   Uses LO offset: No
+|   |   |     _____________________________________________________
+|   |   |    /
+|   |   |   |       RX Codec: B
+|   |   |   |   Name: ads62p48
+|   |   |   |   Gain range digital: 0.0 to 6.0 step 0.5 dB
+|   |     _____________________________________________________
+|   |    /
+|   |   |       TX DSP: 0
+|   |   |   Freq range: -100.000 to 100.000 MHz
+|   |     _____________________________________________________
+|   |    /
+|   |   |       TX DSP: 1
+|   |   |   Freq range: -100.000 to 100.000 MHz
+|   |     _____________________________________________________
+|   |    /
+|   |   |       TX Dboard: A
+|   |   |   ID: CBX (0x0066)
+|   |   |   Serial: 313BF42
+|   |   |     _____________________________________________________
+|   |   |    /
+|   |   |   |       TX Frontend: 0
+|   |   |   |   Name: CBX TX
+|   |   |   |   Antennas: TX/RX, CAL
+|   |   |   |   Sensors: lo_locked
+|   |   |   |   Freq range: 1200.000 to 6000.000 MHz
+|   |   |   |   Gain range PGA0: 0.0 to 31.5 step 0.5 dB
+|   |   |   |   Bandwidth range: 40000000.0 to 40000000.0 step 0.0 Hz
+|   |   |   |   Connection Type: QI
+|   |   |   |   Uses LO offset: No
+|   |   |     _____________________________________________________
+|   |   |    /
+|   |   |   |       TX Codec: A
+|   |   |   |   Name: ad9146
+|   |   |   |   Gain Elements: None
+|   |     _____________________________________________________
+|   |    /
+|   |   |       TX Dboard: B
+|   |   |   ID: CBX (0x0066)
+|   |   |   Serial: 313BF3C
+|   |   |     _____________________________________________________
+|   |   |    /
+|   |   |   |       TX Frontend: 0
+|   |   |   |   Name: CBX TX
+|   |   |   |   Antennas: TX/RX, CAL
+|   |   |   |   Sensors: lo_locked
+|   |   |   |   Freq range: 1200.000 to 6000.000 MHz
+|   |   |   |   Gain range PGA0: 0.0 to 31.5 step 0.5 dB
+|   |   |   |   Bandwidth range: 40000000.0 to 40000000.0 step 0.0 Hz
+|   |   |   |   Connection Type: QI
+|   |   |   |   Uses LO offset: No
+|   |   |     _____________________________________________________
+|   |   |    /
+|   |   |   |       TX Codec: B
+|   |   |   |   Name: ad9146
+|   |   |   |   Gain Elements: None
+
